41a86f0ea714f2cf3c110015f3b13285d138d6a7
hk973 <hariomkankatti57@gmail.com>
2026-06-22 15:51:10 +0530

    version code increased

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.genzopia.Instagame"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "8.0"
+        versionCode = 10
+        versionName = "10.0"
 
         val fileUploadApiKey: String = (project.findProperty("file_upload_api_key") as String?) ?: ""
         buildConfigField("String", "FILE_UPLOAD_API_KEY", "\"$fileUploadApiKey\"")
